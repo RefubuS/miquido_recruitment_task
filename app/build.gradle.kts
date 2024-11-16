bafee8c0efd62bd9a97ac257b9800077aa1741a8
RefubuS <refubuspl@gmail.com>
2024-11-16 18:52:31 +0100

    Add splash screen and launcher icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
     implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
--- app/src/main/java/com/filipzagulak/miquidorecruitmenttask/MainActivity.kt
+++ app/src/main/java/com/filipzagulak/miquidorecruitmenttask/MainActivity.kt
@@ -3,6 +3,7 @@ package com.filipzagulak.miquidorecruitmenttask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.filipzagulak.miquidorecruitmenttask.ui.theme.MiquidoRecruitmentTaskTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         setContent {
             val navController = rememberNavController()
